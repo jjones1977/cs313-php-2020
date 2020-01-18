4264eb12c5634c5e0d28c515c864cfa673fa90c8
jjones1977 <JenniJones9977@gmail.com>
2020-01-18 08:13:01 -0800

    added Prove02 project

---
+++ web/Week2Prove.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prove Week 02</title>
+        <link rel="stylesheet" type="text/css" href="Week2Prove.css">
+    </head>
+<body>
+
+    <div class="row">
+        <div class="column">
+            <div id="div1"><h1>Hawaii</h1></div>
+        </div>
+        <div class="column">
+            <div id="div8">
+                <img src="NaPaliRainbow.jpg" alt="Na Pali with Rainbow" width="300" height=auto>
+            </div>
+        </div>
+        <div class="column">
+            <div id="div2">
+                <img src="ZFRView.jpg" alt="Zack, Finn, Russ" width="144" height=auto>
+            </div>
+            <div id="div5">
+                <img src="Bracken.jpeg" alt="Bracken" width="144" height=auto>
+            </div>
+        </div>
+        <div class="column">
+            <div id="div4">
+                <img src="FinnKayak.jpg" alt="Finn Kayak" width="168" height=auto></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div id="div6">
+                <img src="Tier.jpeg" alt="Tier" width="350" height="150">
+            </div>
+            <div id="div7">
+                <img src="Family.jpg" alt="Family Picture" width="350" height=auto>
+            </div>
+        </div>
+        <div class="column">
+            <div id="div8">
+                <img src="ZKKayak.JPG" alt="Zack Kate Kayak" width="150" height=auto>
+            </div>
+            <div id="div9"><h2>2020</h2></div>
+            <div id="div10">
+                <img src="ZipLine.jpg" alt="Zip Line" width="150" height="118">
+            </div>
+        </div>
+        <div class="column">
+            <div id="div11">
+                <p id="time">
+                <?php
+                    date_default_timezone_set("Pacific/Honolulu");
+                    echo "The time in Hawaii is " . date("h:i a.");
+                ?>
+                </p>
+            </div>
+            <div id="div12">
+                <img src="ZackRock.jpeg" alt="Zack Rock" width="160" height=auto>
+                <img src="FinnFort.JPG" alt="Zack Rock" width="160" height=auto>
+            </div>
+            <div id="div14">
+                <img src="Forest.jpeg" alt="Forest" width="333" height="125">
+            </div>
+        </div>
+        <div class="column">
+            <div id="div14">
+                <img src="NaPali.JPG" alt="Na Pali" width="214" height="142">
+            </div>
+            <div id="div15">
+                <img src="FinnRock.jpeg" alt="Zack Rock" width="214" height="272">
+            </div>
+        </div>
+    </div>
+    <a href="Week2Prove2.php"><p id="Assign">Assignments</p></a>
+</body>
+</html>
---
+++ web/Week2Prove2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Assignment Sheet</title>
+        <link rel="stylesheet" type="text/css" href="Week2Prove.css">
+    </head>
+    <body>
+        <pt>Coming Soon!</pt>
+    </body>
+</html>
